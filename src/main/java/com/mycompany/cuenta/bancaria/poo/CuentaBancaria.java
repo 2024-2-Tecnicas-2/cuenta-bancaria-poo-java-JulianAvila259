a8171f44af3429a1a750460ea4a96c4120680d16
... <scamargomontoya@gmail.com>
2024-09-02 21:36:01 -0500

    Creación de atributos_métodos

--- src/main/java/com/mycompany/cuenta/bancaria/poo/CuentaBancaria.java
+++ src/main/java/com/mycompany/cuenta/bancaria/poo/CuentaBancaria.java
@@ -1,5 +1,62 @@
 package com.mycompany.cuenta.bancaria.poo;
 
+import java.util.Scanner;
+
 public class CuentaBancaria {
     // TODO: Aquí va el código de la clase CuentaBancaria
+    Scanner sc = new Scanner(System.in);
+    private String titular;
+    private String numeroCuenta;
+    private double saldo;
+    private double tipoInteres = 0.015;
+
+    public CuentaBancaria(String titular, String numeroCuenta, double saldo) {
+        this.titular = titular;
+        this.numeroCuenta = numeroCuenta;
+        this.saldo = saldo;
+    }
+
+    public String getTitular() {
+        return titular;
+    }
+
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+
+    public String getNumeroCuenta() {
+        return numeroCuenta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void Ingresar(double cantidad) {
+        cantidad += saldo;
+
+    }
+
+    public void Retirar(double cantidad) {
+        if (cantidad < saldo && cantidad >= 0) {
+            cantidad -= saldo;
+        } else {
+            System.out.println("Saldo insuficiente");
+        }
+    }
+
+    public double calcularInteres(double saldo, double tipoInteres) {
+        return saldo += saldo*(tipoInteres/100);
+    }
+
+    public void setTipoInteres(double tipoInteres) {
+        if (tipoInteres<=10) {
+          this.tipoInteres = tipoInteres;
+        }else {
+            System.out.println("El interes no puede ser mayor al 10%");
+            this.tipoInteres = 10;
+        }
+        
+    }
+    
 }
--- src/main/java/com/mycompany/cuenta/bancaria/poo/Principal.java
+++ src/main/java/com/mycompany/cuenta/bancaria/poo/Principal.java
@@ -3,5 +3,10 @@ package com.mycompany.cuenta.bancaria.poo;
 public class Principal {
     public static void main(String[] args) {
         // TODO: Adiciona aquí el código que deseas para la Cuenta Bancaria.
+        CuentaBancaria c1 = new CuentaBancaria("Julian", "123456789", 2000000);
+        System.out.println(c1.calcularInteres(2000000, 8));
+        
+         CuentaBancaria c2 = new CuentaBancaria("Diego", "123456788", 2000000);
+        System.out.println(c1.calcularInteres(2000000, 15));
     }
 }
